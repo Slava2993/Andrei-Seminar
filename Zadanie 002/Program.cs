60d7a09fd365247e176fb0f2f776b08e823cb7f7
slava <shevaslava1993@gmail.com>
2022-10-20 22:29:26 +0300

    Zadanie 002

--- Zadanie 002/Program.cs
+++ Zadanie 002/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи свое первое число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи свое второе число - ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+Console.WriteLine($"Большее {a} меньшее {b}");
+}
+if (a < b)
+{
+Console.WriteLine($"Меньшее {a} Большее {b}");
+}
