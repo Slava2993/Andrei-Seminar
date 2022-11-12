eaeda4d8f2cc36271f668f392a2f821230483f02
slava <shevaslava1993@gmail.com>
2022-11-12 23:22:51 +0300

    Zadanie 23

---
+++ Zadanie 23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int num)
+{
+if (num > 0)
+{
+for (int i = 1; i <= num; i++)
+{
+Console.WriteLine($"|{i,4} {i * i * i,4}");
+}
+}
+else
+{
+Console.WriteLine("Введите значение больше 0");
+}
+}
+
+SquareTable(n);
